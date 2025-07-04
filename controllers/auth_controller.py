6b11d064c296e95faeb68895b617fe7c283c3fce
23f2005609 <23f2005609@ds.study.iitm.ac.in>
2025-07-04 22:15:22 +0530

    Milestone-VP Auth-RBAC

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -4,44 +4,44 @@ from flask import current_app as app
 
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def home_page():
+    return render_template("home.html")
 
 @app.route("/login", methods=["GET","POST"])
-def signin():
+def user_signin():
     if request.method=="POST":
-        #retirives form values from login form
-        uname=request.form.get("user_name") 
+        #it retirives form values from login form
+        u_name=request.form.get("user_name") 
         pwd=request.form.get("password")
-        usr=UserInfo.query.filter_by(email=uname,password=pwd).first() 
-        if usr and usr.id==1: #Existed and i am assuming the first registerd will be the admin the users
-            return redirect(url_for("admin_dashboard",email=uname))
+        usr=UserInfo.query.filter_by(email=u_name,password=pwd).first() #it matches values from form does it exists or not
+        if usr and usr.id==1: #Existed and the first registerd will be the admin that we will auto create
+            return redirect(url_for("admin_dash",email=u_name))
         elif usr:
-            return redirect(url_for("user_dashboard",email=uname))
+            return redirect(url_for("user_dash",email=u_name))
         else:
-            flash("Invalid user credentials...", "danger")
-            return redirect(url_for("signin"))
+            flash("Incorrect User Details!, try again..", "danger")
+            return redirect(url_for("user_signin"))
         
     return render_template("login.html")
 
 @app.route("/register", methods=["GET","POST"]) #GET- while showing form and POST- while submiting form
-def signup():
+def user_signup():
     if request.method=="POST":
         #gets input from registration form
-        uname=request.form.get("user_name")
+        u_name=request.form.get("user_name")
         pwd=request.form.get("password")
         fullname=request.form.get("fullname")
         address=request.form.get("address")
-        pin=request.form.get("pincode")
-        usr=UserInfo.query.filter_by(email=uname,password=pwd).first()
+        pin=request.form.get("pin_code")
+        usr=UserInfo.query.filter_by(email=u_name,password=pwd).first() # we are checking if user already exists or not
         if usr:
             flash("This email is already registered, try login now!", "danger")
-            return redirect(url_for("signin"))
+            return redirect(url_for("user_signin"))
         #creates new userinfo and saves it to db
-        new_usr=UserInfo(email=uname,password=pwd,fullname=fullname,address=address,pin_code=pin)
+        new_usr=UserInfo(email=u_name,password=pwd,fullname=fullname,address=address,pin_code=pin)
         db.session.add(new_usr)
         db.session.commit()
-        flash("Registration Successfull, try login now!", "success")
-        return redirect(url_for("signin"))
+        flash("Registration is Successfull, lets login!", "success")
+        return redirect(url_for("user_signin"))
     return render_template("signup.html")
 
