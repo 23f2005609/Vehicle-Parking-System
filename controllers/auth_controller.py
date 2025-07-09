7f483ce0a87051fe0ac9b276769b08fb9995744b
23f2005609 <23f2005609@ds.study.iitm.ac.in>
2025-07-10 00:38:38 +0530

    Milestone-VP Auth-RBAC

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -3,27 +3,29 @@ from models.models import *
 from flask import current_app as app
 
 
+
 @app.route("/")
 def home_page():
     return render_template("home.html")
 
-@app.route("/login", methods=["GET","POST"])
+@app.route("/login", methods=["GET", "POST"])
 def user_signin():
-    if request.method=="POST":
-        #it retirives form values from login form
-        u_name=request.form.get("user_name") 
-        pwd=request.form.get("password")
-        usr=UserInfo.query.filter_by(email=u_name,password=pwd).first() #it matches values from form does it exists or not
-        if usr and usr.id==1: #Existed and the first registerd will be the admin that we will auto create
-            return redirect(url_for("admin_dash",email=u_name))
-        elif usr:
-            return redirect(url_for("user_dash",email=u_name))
+    if request.method == "POST":
+        u_name = request.form.get("user_name") 
+        pwd = request.form.get("password")
+        # Find user by email
+        usr = UserInfo.query.filter_by(email=u_name).first()
+        # If user exists, check the hashed password is
+        if usr and usr.password == pwd:
+            if usr.id == 1:  # First user is Admin
+                return redirect(url_for("admin_dash", email=u_name))
+            else:
+                return redirect(url_for("user_dash", email=u_name))
         else:
             flash("Incorrect User Details!, try again..", "danger")
             return redirect(url_for("user_signin"))
-        
-    return render_template("login.html")
 
+    return render_template("login.html")
 @app.route("/register", methods=["GET","POST"]) #GET- while showing form and POST- while submiting form
 def user_signup():
     if request.method=="POST":
